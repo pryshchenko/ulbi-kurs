a8288c802892ff22d0e729d3f078002671d3aa48
pryshchenko <pryshchenko.d@gmail.com>
2023-08-18 15:23:26 +0300

    added webpack-refresh-plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,10 +2,11 @@ import HTMLWebpackPlugin from 'html-webpack-plugin'
 import webpack, { WebpackPluginInstance } from 'webpack'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { BuildOptions } from './types/config'
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 
 export function buildPlugins({paths, isDev}: BuildOptions): WebpackPluginInstance[] {
 
-  return [
+    const plugins = [
     new HTMLWebpackPlugin({
       template: paths.html
     }),
@@ -16,6 +17,13 @@ export function buildPlugins({paths, isDev}: BuildOptions): WebpackPluginInstanc
     }),
     new webpack.DefinePlugin({
         __IS_DEV__: JSON.stringify(isDev)
-    })
+    }),
   ]
+  
+  if (isDev) {
+    plugins.push(new ReactRefreshWebpackPlugin());
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+  }
+
+  return plugins;
 }
