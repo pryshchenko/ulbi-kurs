2f08a8626393d250a11c62eaed5b250b4aa9fee2
pryshchenko <pryshchenko.d@gmail.com>
2023-09-09 03:11:38 +0300

    Storybook нет доступа к глобальным переменным css

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,10 @@ module.exports = {
         "es2021": true
     },
     "extends": [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:i18next/recommended'
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:i18next/recommended",
+        "plugin:storybook/recommended"
     ],
     "overrides": [
         {
